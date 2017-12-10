9a124c4fe77759e53e448c551a1af23170913d48
Igor_Zh <zhizhkaigor@gmail.com>
2017-12-10 14:14:45 +0200

    test

--- src/main/java/Service/SenderSms.java
+++ src/main/java/Service/SenderSms.java
@@ -14,7 +14,7 @@ public class SenderSms {
 	
 	
 	
-	@Scheduled(cron="*/5 * * * * *")
+	@Scheduled(cron="1 30 3 * * *")
 	public void senderer() {
 
 		String hourLeter = LocalDateTime.now().plusHours(1).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
@@ -23,5 +23,9 @@ public class SenderSms {
 //        sd.sendSms("380677256482", "Привет Юрий: " + hourLeter , 1, "", "", 0, "", "");
 //        sd.getSmsCost("380677256482", "Вы успешно зарегистрированы!", 0, 0, "", "");
 //        sd.getBalance();
+      sd.sendSms("380503718360", "Привет: " + hourLeter , 1, "", "", 0, "", "");
+      sd.getSmsCost("380503718360", "Test Heroku", 0, 0, "", "");
+      sd.getBalance();
+
 	}
 }
